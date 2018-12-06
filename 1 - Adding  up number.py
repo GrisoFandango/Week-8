8b4a9df12df9dbb4cb26060993ca2f47097c984f
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-12-06 17:46:38 +0000

    Update 1 - Adding  up number.py
    adding comments

--- 1 - Adding  up number.py
+++ 1 - Adding  up number.py
@@ -1,19 +1,23 @@
 sum=0
-#remember that range will stop a number before the upper limit, in this case 9
+#remember that range will stop a number before the upper limit, 
+#in this case 9
     sum += i
+#This loop will give i valuje from 1 to 9
 for i in range(1,10):
     sum += i
 
 print("Sum from 1 to 10 is", sum)
 
 sum=0
-for i in range(20,37):
+#This loop will give i value from 20 to 37
+for i in range(20,38):
     sum += i
 
 print("Sum from 20 to 37 is", sum)
 
 sum=0
-for i in range(35,49):
+#This loop will give i value from 35 to 49
+for i in range(35,50):
     sum += i
 
 print("Sum from 35 to 49 is", sum)
